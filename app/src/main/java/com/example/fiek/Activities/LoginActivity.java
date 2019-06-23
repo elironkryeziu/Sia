c86dc405de4a799a52f5a4ae8872fe2918768e78
Eliron <eliron168@gmail.com>
2019-06-23 14:54:02 +0200

    First commit

---
+++ app/src/main/java/com/example/fiek/Activities/LoginActivity.java
@@ -0,0 +1,110 @@
+package com.example.fiek.Activities;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fiek.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText userEmail, userPassword;
+    private Button btnLogin;
+    private ProgressBar loginProgress;
+    private FirebaseAuth mAuth;
+    private Intent HomeActivity;
+    private Intent RegisterActivity;
+    private TextView txtRegister;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        userEmail = findViewById(R.id.login_mail);
+        userPassword = findViewById(R.id.login_password);
+        btnLogin = findViewById(R.id.btnLogin);
+        loginProgress = findViewById(R.id.loadingProgress);
+        txtRegister = findViewById(R.id.txtRegister);
+        mAuth = FirebaseAuth.getInstance();
+        HomeActivity = new Intent(this, com.example.fiek.Activities.Home.class);
+        RegisterActivity = new Intent(this,com.example.fiek.Activities.RegisterActivity.class);
+
+        loginProgress.setVisibility(View.INVISIBLE);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginProgress.setVisibility(View.VISIBLE);
+                btnLogin.setVisibility(View.INVISIBLE);
+
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                if (email.isEmpty() || password.isEmpty()){
+                    showMessage("Ju lutem plotesoni te dhenat");
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    btnLogin.setVisibility(View.VISIBLE);
+                }
+                else{
+                    signIn(email,password);
+                }
+            }
+        });
+
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(RegisterActivity);
+                finish();
+            }
+        });
+    }
+
+    private void signIn(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    btnLogin.setVisibility(View.VISIBLE);
+                    updateUI();
+                }
+
+            }
+        });
+
+    }
+
+    private void updateUI() {
+        startActivity(HomeActivity);
+        finish();
+    }
+
+    private void showMessage(String meesage) {
+        Toast.makeText(getApplicationContext(),meesage,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null){
+            updateUI();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fiek/Activities/RegisterActivity.java
@@ -0,0 +1,223 @@
+package com.example.fiek.Activities;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.fiek.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    static int PReqCode = 1;
+    static int REQUESCODE = 1;
+    Uri pickedImage;
+    ImageView ImgUserPhoto;
+    private EditText userEmail, userPassword, userPassword2, userName;
+    private ProgressBar loadingProgress;
+    private Button regBtn;
+
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        userEmail = findViewById(R.id.regEmail);
+        userPassword = findViewById(R.id.regPassword);
+        userPassword2 = findViewById(R.id.regPassword2);
+        userName = findViewById(R.id.regName);
+        loadingProgress = findViewById(R.id.progressBar);
+        regBtn = findViewById(R.id.regBtn);
+        ImgUserPhoto = findViewById(R.id.regUserPhoto);
+
+        loadingProgress.setVisibility(View.INVISIBLE);
+        mAuth = FirebaseAuth.getInstance();
+
+        ImgUserPhoto.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(Build.VERSION.SDK_INT >= 22){
+
+                    checkAndRequestForPermission();
+                }
+                else{
+                    openGallery();
+                }
+
+            }
+        });
+
+        regBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                regBtn.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.VISIBLE);
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                final String password2 = userPassword2.getText().toString();
+                final String name = userName.getText().toString();
+
+                if(!password.equals(password2)){
+                    showMessage("Gabim gjate konfirmimit te passwordit");
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+                }
+                else if(email.isEmpty() || password.isEmpty() || name.isEmpty()){
+                    showMessage("Plotesoni te gjitha fushat");
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    createUserAccount(email,name,password);
+
+                }
+
+            }
+        });
+
+    }
+
+
+    private void openGallery() {
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
+
+    }
+
+    private void checkAndRequestForPermission() {
+
+        if (ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+                showMessage("Ju lutem pranojeni kerkesen");
+            }
+            else{
+                ActivityCompat.requestPermissions(RegisterActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PReqCode);
+            }
+        }
+        else{
+            openGallery();
+        }
+
+
+    }
+
+    private void createUserAccount(String email, final String name, String password) {
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            showMessage("Useri eshte regjistruar me sukses");
+                            updateUserInfo(name,pickedImage,mAuth.getCurrentUser());
+                        }
+                        else{
+                            showMessage("Ka ndodhur nje gabim gjate regjistrimit te userit");
+                            regBtn.setVisibility(View.VISIBLE);
+                            loadingProgress.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+
+    }
+
+    private void updateUserInfo(final String name, final Uri pickedImage, final FirebaseUser currentUser) {
+
+
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("users_photos");
+        final StorageReference imageFilePath = mStorage.child(pickedImage.getLastPathSegment());
+        imageFilePath.putFile(pickedImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+
+                        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .setPhotoUri(uri)
+                                .build();
+
+                        currentUser.updateProfile(profileUpdate)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()){
+                                            showMessage("Regjistrimi u krye me sukses");
+                                            updateUI();
+                                        }
+                                    }
+                                });
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void updateUI() {
+
+        Intent homeActivity = new Intent(getApplicationContext(),Home.class);
+        startActivity(homeActivity);
+        finish();
+
+
+
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUESCODE && data != null){
+
+            //the user has succesfully picked an image
+            //we need to save it to an URI variable
+            pickedImage = data.getData();
+            ImgUserPhoto.setImageURI(pickedImage);
+
+        }
+
+    }
+
+
+}
+
